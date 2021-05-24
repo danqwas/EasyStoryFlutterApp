597a00fd62a2a7c6b83e11e52d2bb3aae226cadd
ElioCampos <eliosalva91@gmail.com>
2021-05-23 19:46:59 -0500

    add app base template

---
+++ lib/src/pages/hashtags_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HashtagPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hashtags Page'),
+      ),
+    );
+  }
+}
