334ca52efc235845603709eb033f36abbc9ab15d
ElioCampos <eliosalva91@gmail.com>
2021-05-24 09:57:22 -0500

    add post details view

--- lib/src/pages/post_page.dart
+++ lib/src/pages/post_page.dart
@@ -1,60 +1,127 @@
 import 'package:flutter/material.dart';
 
-class PostPage1 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Card(
-        child: TextField(
-          maxLines: 40,
-          minLines: 10,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'Escribe una publicación',
-          ),
-        ),
-      ),
-    );
-  }
-}
-
 class PostPage extends StatefulWidget {
-  //PostPage({Key key}) : super(key: key);
   @override
   _PostPageState createState() => _PostPageState();
 }
 
 class _PostPageState extends State<PostPage> {
+  TextEditingController postText = new TextEditingController();
+  bool isValid = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(children: <Widget>[
         Divider(),
         TextField(
+          controller: postText,
           maxLines: 40,
           minLines: 10,
           decoration: InputDecoration(
             border: OutlineInputBorder(),
             labelText: 'Escribe una publicación',
+            hintText: 'Contenido',
+            errorText: isValid ? 'La publicación no puede estar vacía' : null,
           ),
         ),
-        ElevatedButton(onPressed: () {}, child: const Text('Siguiente'))
+        Divider(),
+        Center(
+          child: ElevatedButton(
+            onPressed: () {
+               setState(() {
+                  postText.text.isEmpty ? isValid = true : isValid = false;
+                });
+              if (!isValid) {
+                Navigator.push(context,
+                   MaterialPageRoute(builder: (context) => AddDetails()),
+               );
+              }
+            }, 
+            child: const Text('Siguiente'),
+          ),
+        ),
       ]),
+    );
+  }
+}
 
-      /*body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Divider(),
-            TextField(
-              maxLines: 40,
-              minLines: 10,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Escribe una publicación',
+class AddDetails extends StatefulWidget {
+  @override
+  _AddDetailsState createState() => _AddDetailsState();
+}
+
+class _AddDetailsState extends State<AddDetails> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Agregar detalles'),
+      ),
+      body: ListView(children: <Widget>[
+        Divider(),
+        TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Inserte título',
+            hintText: 'Título'
+          ),
+        ),
+        Divider(),
+         TextField(
+          maxLines: 10,
+          minLines: 7,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Inserte descripción',
+            hintText: 'Descripción'
+          ),
+        ),
+        Divider(),
+        Center(
+          child: ElevatedButton(
+            onPressed: () { Navigator.pop(context); },
+            child: const Text('Editar publicación')
+          )
+        ),
+        Divider(),
+        Row(children: <Widget>[
+          Checkbox(
+          checkColor: Colors.white,
+          value: isChecked,
+          onChanged: (bool value) {
+            setState(() {
+              isChecked = value;
+            });
+          },),
+          Text('Comprobación de originalidad')
+        ],),
+        Divider(),
+        Center(
+          child: ElevatedButton(
+            onPressed: () { showDialog<String>(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+                title: const Text('¿Seguro que deseas publicar?'),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text('Regresar'),
+                  ),
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text('Publicar'),
+                  ),
+                ],
               ),
-            ),
-            ElevatedButton(onPressed: () {}, child: const Text('Siguiente'))
-          ]),*/
+            ); 
+          },
+            child: const Text('Publicar')
+          )
+        ),
+      ]),
     );
   }
 }
